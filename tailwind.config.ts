7aabefb11de5cc4a5f0319bc1e05ac1f32eb69bc
jorgebecerril <jorgebecerrilgm@gmail.com>
2024-12-30 16:44:11 -0600

    Add Roboto family and xss font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     extend: {
       fontFamily: {
         sans: [
+          '"Roboto"',
           '"Inter"',
           "ui-sans-serif",
           "system-ui",
@@ -16,6 +17,9 @@ export default {
           '"Noto Color Emoji"',
         ],
       },
+      fontSize: {
+        xxs: ['0.6rem', '0.6rem'],
+      }
     },
   },
   plugins: [],
