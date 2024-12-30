5c8f2e28e26199da2d37ecff78ae2074b6437520
jorgebecerril <jorgebecerrilgm@gmail.com>
2024-12-30 01:57:03 -0600

    Remove junk from template

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Jorge Becerril" },
+    { name: "description", content: "My personal portfolio." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <h1>Home</h1>;
 }
