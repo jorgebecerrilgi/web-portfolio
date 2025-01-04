1ae87afce7e7392ca48923f2f4f72d9ed8da1f81
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-03 18:14:34 -0600

    Scaled footer up

--- app/homepage/footer.tsx
+++ app/homepage/footer.tsx
@@ -4,9 +4,9 @@ import { MdEmail } from "react-icons/md";
 const Footer = () => {
     return (
         <div className="flex gap-2 items-end">
-            <a href="mailto:jorgebecerrilgm@gmail.com" aria-label="email"><MdEmail size={12}/></a>
-            <a href="https://www.linkedin.com/in/jorgebecerril" aria-label="linkedin"><FaLinkedinIn size={12}/></a>
-            <a href="https://www.github.com/jorgebecerrilgi" aria-label="github"><FaGithub size={11}/></a>
+            <a href="mailto:jorgebecerrilgm@gmail.com" aria-label="email"><MdEmail size={14}/></a>
+            <a href="https://www.linkedin.com/in/jorgebecerril" aria-label="linkedin"><FaLinkedinIn size={14}/></a>
+            <a href="https://www.github.com/jorgebecerrilgi" aria-label="github"><FaGithub size={13}/></a>
         </div>
     );
 };
