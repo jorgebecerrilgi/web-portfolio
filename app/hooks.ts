514c9c5de45e3505e15f91c976c84556fb34067b
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-03 18:12:52 -0600

    Add useQueue and useTimeoutEffect hooks

--- app/hooks.ts
+++ app/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export const useIntervalEffect = (callbackfn: () => void, ms?: number) => {
   const currentfn = useRef(callbackfn);
@@ -37,3 +37,42 @@ export const useMediaQuery = (query: string) => {
 
   return matches;
 };
+
+export const useQueue = <T>(initialItems: T[] = []): {
+  push: (item: T) => void;
+  pop: () => T | undefined;
+  top: () => T | undefined;
+  items: T[];
+} => {
+  const [queue, setQueue] = useState<T[]>(initialItems);
+
+  const push = useCallback((item: T) => setQueue(prev => [...prev, item]), []);
+
+  const pop = useCallback(() => {
+    const [front, ...remaining] = queue;
+    setQueue(remaining);
+    return front;
+  }, [queue]);
+
+  const top = useCallback(() => queue[0], [queue]);
+
+  return { push, pop, top, items: queue };
+}
+
+export const useTimeoutEffect = (callbackfn: () => void, ms?: number, deps: any[] = []) => {
+  const currentfn = useRef(callbackfn);
+
+  useEffect(() => {
+    currentfn.current = callbackfn;
+  }, [callbackfn]);
+  
+  useEffect(() => {
+    const fn = () => {
+      currentfn.current();
+    };
+    if (ms !== undefined) {
+      const timeout = setTimeout(fn, ms);
+      return () => clearTimeout(timeout);
+    }
+  }, [ms, ...deps]);
+}
