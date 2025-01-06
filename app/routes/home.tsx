8c94c668e2e71780c3ef68a71e988e0d9b2b03f1
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-06 16:43:11 -0600

    Update home route components from function to arrow function components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,19 @@
 import Homepage from "~/homepage";
 import type { Route } from "./+types/home";
 
-export function meta({}: Route.MetaArgs) {
+export const meta = ({}: Route.MetaArgs) => {
   return [
     { title: "Jorge Becerril" },
     { name: "description", content: "My personal portfolio." },
   ];
 }
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="w-1/2 aspect-[10/17] sm:aspect-[17/10]">
       <Homepage/>
     </div>
   );
 }
+
+export default Home;
