e71b6859bca9a767e802dc078418c34bdfe87b4a
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-08 14:51:30 -0600

    Move projects layout to /projects/ directory

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
     index("routes/home.tsx"),
-    layout("routes/layout.tsx", [
+    layout("projects/layout.tsx", [
         route("about-me", "routes/about-me.tsx"),
     ]),
 ] satisfies RouteConfig;
