23d252de23858881742c189b7e342be4f001133a
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-08 20:17:52 -0600

    Create Details component for project details

---
+++ app/projects/details.tsx
@@ -0,0 +1,31 @@
+interface DetailsData {
+    titles: string[],
+    content: string[][],
+}
+
+interface DetailsProps {
+    data: DetailsData,
+}
+
+const Details: React.FC<DetailsProps> = ({ data }) => {
+    const { titles, content } = data;
+    
+    return (
+        <>{
+            titles.map((title, i) => {
+                const items = content[i];
+                return (
+                    <div className="mb-[24px]" key={title}>
+                        <div className="h-[2px] bg-neutral-950"></div>
+                        <div className="flex justify-between">
+                            <p>{title}</p>
+                            <div className="text-end">{items.map(item => <p key={item}>{item}</p>)}</div>
+                        </div>
+                    </div>
+                );
+            })
+        }</>
+    );
+};
+
+export default Details;
