1630b89428d8fadec199231163360ceed31d097a
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-08 21:21:10 -0600

    Fix footer z-index issues

--- app/homepage/footer.tsx
+++ app/homepage/footer.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { FOOTER_ICONS } from "./constants";
+import { useTimeoutEffect } from "~/hooks";
 
 const Footer = () => {
+    const [show, setShow] = useState(false);
+
+    useTimeoutEffect(() => setShow(true), 200);
+    
     return (
-        <footer className="flex gap-2 items-end motion-opacity-in-0 motion-delay-200">
+        <footer className={`flex gap-4 items-end duration-200 ${show ? "" : "opacity-0"}`}>
             {
                 FOOTER_ICONS.map(({ href, icon, "aria-label": ariaLabel }) => (
                     <a href={href} aria-label={ariaLabel} target="_blank" rel="noopener noreferrer" key={href}>
