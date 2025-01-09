f09867691c3ca0218526742f2e95fb6dd44ae5dc
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-08 20:17:15 -0600

    Add size prop for MotionEnter component

--- app/projects/motion-enter.tsx
+++ app/projects/motion-enter.tsx
@@ -1,10 +1,14 @@
 interface MotionEnterProps {
     children: React.ReactNode;
+    size?: "small" | "large";
 }
 
-const MotionEnter: React.FC<MotionEnterProps> = ({ children }) => {
+const MotionEnter: React.FC<MotionEnterProps> = ({ size = "small" , children }) => {
     return (
-        <div className="py-3 intersect:motion-opacity-in-0 intersect:motion-translate-y-in-[10%] motion-delay-200">
+        <div className={`
+            intersect:motion-opacity-in-0 intersect:motion-translate-y-in-[10%] motion-delay-200
+            ${size === "small" ? "py-4" : "py-14"}
+        `}>
             {children}
         </div>
     );
