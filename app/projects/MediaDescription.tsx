2149142b1dd543aeb718bf3862f73f86b6b53782
jorgebecerril <jorgebecerrilgm@gmail.com>
2025-01-09 21:57:56 -0600

    Create MediaDescription component to display videos or images with a description

---
+++ app/projects/MediaDescription.tsx
@@ -0,0 +1,33 @@
+interface MediaDescriptionProps {
+    type: "video" | "image";
+    src: string;
+    placeText?: "start" | "end";
+    alt?: string;
+    children?: React.ReactNode;
+}
+
+const MediaDescription: React.FC<MediaDescriptionProps> = ({ type, src, placeText= "start", alt, children }) => {
+    const media = type === "image"
+        ? <img src={src} alt={alt}/>
+        : <video src={src} autoPlay loop muted />;
+    
+    if (!children) return media;
+    
+    if (placeText === "start") {
+        return (
+            <div className="grid md:grid-cols-[0.225fr_1fr] items-end justify-items-end gap-5">
+                <p className="row-start-2 max-w-40 md:row-start-1 md:max-w-full text-end leading-tight">{children}</p>
+                {media}
+            </div>
+        );
+    }
+    
+    return (
+        <div className="grid grid-cols-[1fr_0.225fr] items-end gap-5">
+            {media}
+            <p className="leading-tight">{children}</p>
+        </div>
+    );
+};
+
+export default MediaDescription;
